d5a178db41a5ed610dc2b4b5f838d45d209e10ab
guylmaster <guylmaster@gmail.com>
2020-03-26 08:45:06 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,13 +3,15 @@ unset DEBIAN_FRONTEND
 export LOG=/var/log/jitsi/jvb.log
 
 if [ ! -f "$LOG" ]; then
-	
 	sed 's/#\ create\(.*\)/echo\ create\1 $JICOFO_AUTH_USER $JICOFO_AUTH_DOMAIN $JICOFO_AUTH_PASSWORD/' -i /var/lib/dpkg/info/jitsi-meet-prosody.postinst
 
-	dpkg-reconfigure jitsi-videobridge
+	/autoconf.videobridge.exp ${JITSI_HOSTNAME}
+
 	rm /etc/jitsi/jicofo/config && dpkg-reconfigure jicofo
-	/var/lib/dpkg/info/jitsi-meet-prosody.postinst configure
-	dpkg-reconfigure jitsi-meet
+
+        /var/lib/dpkg/info/jitsi-meet-prosody.postinst configure
+
+        /autoconf.jitsimeet.exp
 
 	touch $LOG && \
 	chown jvb:jitsi $LOG
